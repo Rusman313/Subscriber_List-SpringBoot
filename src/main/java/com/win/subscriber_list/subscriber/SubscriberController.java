c846c09e59ae6790267c98770d8c6681e6e81f28
Rusman313 <62064583+Rusman313@users.noreply.github.com>
2020-06-22 10:55:12 -0400

    Created a repository for Subscriber and added a method to actually post data from database

--- src/main/java/com/win/subscriber_list/subscriber/SubscriberController.java
+++ src/main/java/com/win/subscriber_list/subscriber/SubscriberController.java
@@ -1,14 +1,28 @@
 package com.win.subscriber_list.subscriber;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 //Designates this class as a Controller
 @Controller
 public class SubscriberController {
+    @Autowired
+    private SubscriberRepository SubscriberRepository;
+
     // Tells our application what template to return at a specific URL
     @GetMapping
     public String index(Subscriber subscriber){       
         return "subscriber/index";
+    }
+
+    private Subscriber subscriber;
+    public String addNewSubscriber(Subscriber subscriber, Model model){
+        SubscriberRepository.save(new Subscriber(subscriber.getFirstName(), subscriber.getLastName(), subscriber.getUserName(), subscriber.getSignedUp()));
+        model.addAttribute("firstName", subscriber.getFirstName());
+        model.addAttribute("lastName", subscriber.getLastName());
+        model.addAttribute("userName", subscriber.getUserName())
+        return "subscriber/result";
     }
 }
---
+++ src/main/java/com/win/subscriber_list/subscriber/SubscriberRepository.java
@@ -0,0 +1,7 @@
+package com.win.subscriber_list.subscriber;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface SubscriberRepository extends CrudRepository {
+    
+}
